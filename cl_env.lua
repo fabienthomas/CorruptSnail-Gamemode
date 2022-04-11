be81ff6188b3472f4b032f8a2ada03309b7786e5
Fabien THOMAS <mistaputt@gmail.com>
2022-04-12 00:00:11 +0200

    Update cl_env.lua
    updated blackout natives
    allow 3PP in vehicles

--- cl_env.lua
+++ cl_env.lua
@@ -1,6 +1,9 @@
+local isInVehicle = false
 Utils.CreateLoadedInThread(function()
-    DisplayRadar(not Config.HIDE_RADAR)
-    SetBlackout(Config.ENABLE_BLACKOUT)
+	DisplayRadar(not Config.HIDE_RADAR)
+	Citizen.InvokeNative(0xE2B187C0939B3D32, 0)
+	SetArtificialLightsState(true)
+	
     SetAudioFlag("DisableFlightMusic", true)
     SetAudioFlag("PoliceScannerDisabled", true)
     StartAudioScene("FBI_HEIST_H5_MUTE_AMBIENCE_SCENE")
@@ -22,6 +25,10 @@ Utils.CreateLoadedInThread(function()
         HideHudComponentThisFrame(1)
         HideHudComponentThisFrame(3)
         HideHudComponentThisFrame(4)
+		HideHudComponentThisFrame(6)
+		HideHudComponentThisFrame(7)
+		HideHudComponentThisFrame(8)
+		HideHudComponentThisFrame(9)
         HideHudComponentThisFrame(13)
         
         if not Config.ENABLE_PEDS then
@@ -40,7 +47,24 @@ Utils.CreateLoadedInThread(function()
         end        
 
         if Config.FIRST_PERSON_LOCK then
-            DisableControlAction(0, 0, true)
+			local ped = PlayerPedId()
+			if not isInVehicle then
+				if DoesEntityExist(GetVehiclePedIsTryingToEnter(ped)) and not isEnteringVehicle then
+					isEnteringVehicle = true
+				elseif not DoesEntityExist(GetVehiclePedIsTryingToEnter(ped)) and not IsPedInAnyVehicle(ped, true) and isEnteringVehicle then
+					isEnteringVehicle = false
+				elseif IsPedInAnyVehicle(ped, false) then
+					isEnteringVehicle = false
+					isInVehicle = true
+				end
+				DisableControlAction(0, 0, true)
+			elseif isInVehicle then
+				if not IsPedInAnyVehicle(ped, false) or IsPlayerDead(PlayerId()) then
+					isInVehicle = false
+					SetFollowPedCamViewMode(4)
+					SetFollowVehicleCamViewMode(4)
+				end
+			end
         end
     end
 end)
